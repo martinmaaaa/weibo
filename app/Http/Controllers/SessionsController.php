90cc1ca5adfbd7b062c9d2d5c90b4e47f3d5dfeb
macong <martinmaaaa@gmail.com>
2019-11-30 12:49:58 +0000

    退出登录功能

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -27,4 +27,11 @@ class SessionsController extends Controller
     		return redirect()->back()->withInput();
     	}
     }
+
+    public function destroy()
+    {
+    	Auth::logout();
+    	session()->flash('success','您已退出登录');
+    	return redirect('login');
+    }
 }
